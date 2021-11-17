ebfd9af390c9f78d6346de8abcb7768d3f8db1c2
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-11-17 11:13:20 -0600

    create router

---
+++ controllers/LoginController.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace Controllers;
+
+class LoginController {
+    public static function login() {
+        echo 'desde login';
+    }
+    
+    public static function logout() {
+        echo 'desde logout';
+    }
+    
+    public static function resetPassword() {
+        echo 'desde resetPassword';
+    }
+    
+    public static function recoverPassword() {
+        echo 'desde recoverPassword';
+    }
+    
+    public static function crear() {
+        echo 'desde crear cuenta';
+    }
+}
--- public/index.php
+++ public/index.php
@@ -2,10 +2,25 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 
 $router = new Router();
 
+// Iniciar Sesión
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+$router->get('/logout', [LoginController::class, 'logout']);
+
+// Recuperar Password
+$router->get('/resetPassword', [LoginController::class, 'resetPassword']);
+$router->post('/resetPassword', [LoginController::class, 'resetPassword']);
+$router->get('/recoverPassword', [LoginController::class, 'recoverPassword']);
+$router->post('/recoverPassword', [LoginController::class, 'recoverPassword']);
+
+// Crear cuenta
+$router->get('/crear-cuenta', [LoginController::class, 'crear']);
+$router->post('/crear-cuenta', [LoginController::class, 'crear']);
 
 
 
