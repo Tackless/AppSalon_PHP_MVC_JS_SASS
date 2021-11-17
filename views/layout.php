16cafd1cf1e342d163de099bcff1c20fc3451807
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-11-17 11:55:31 -0600

    add views

--- views/layout.php
+++ views/layout.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>App Salón</title>
+    <meta name="description" content="Project with MVC, PHP, SASS, Gulp, Composer">
+    <link rel="shortcut icon" href="/build/img/favicon.ico" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="build/css/app.css">
+    <title>App Salón</title>
 </head>
 <body>
 
