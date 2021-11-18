bc0a84402c034436bb7abd914f217e2bb2b516c0
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-11-17 23:04:39 -0600

    sync to usuario

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers;
 
+use Model\Usuario;
 use MVC\Router;
 
 class LoginController {
@@ -29,10 +30,16 @@ class LoginController {
     
     public static function crear(Router $router) {
         
+        $usuario = new Usuario;
 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+            $usuario->sincronizar($_POST);
+
+        }
         
         $router->render('/auth/crear-cuenta', [
-
+            'usuario' => $usuario
         ]);
     }
 }
