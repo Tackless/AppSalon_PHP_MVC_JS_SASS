7448292a2e578b468d7ba8e95ebf3ca1f51f48b8
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-11-18 11:31:40 -0600

    create template alertas

--- views/auth/crear-cuenta.php
+++ views/auth/crear-cuenta.php
@@ -1,6 +1,10 @@
 <h1 class="nombre-pagina">Crear Cuenta</h1>
 <p class="descripcion-pagina">Llena el siguiente formulario para crear una cuenta</p>
 
+<?php 
+    include_once __DIR__ . "/../templates/alertas.php"
+?>
+
 <form class="formulario" action="/crear-cuenta" method="post">
     <div class="campo">
         <label for="nombre">Nombre:</label>
