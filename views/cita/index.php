824aef50e4ed303990a0790781ec4235c23af307
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-11-26 17:07:49 -0600

    section & name

--- views/cita/index.php
+++ views/cita/index.php
@@ -1,2 +1,37 @@
 <h1 class="nombre-pagina">Crear Cita</h1>
-<p class="descripcion-pagina">Elige tus servicios a continuacion.</p>
+<p class="descripcion-pagina">Elige tus servicios y coloca tus datos.</p>
+
+<div id="app">
+    <div id="paso-1" class="seccion">
+        <h2>Servicios</h2>
+        <p class="text-center">Elige tus servicios a continuación</p>
+        <div id="servicios" class="listado-servicios"></div>
+    </div>
+    
+    <div id="paso-2" class="seccion">
+        <h2>Tus datos y cita</h2>
+        <p class="text-center">Coloca tus datos y fecha de tu cita</p>
+
+        <form class="formulario">
+            <div class="campo">
+                <label for="nombre">Nombre</label>
+                <input type="text" id="nombre" placeholder="Tu Nombre" value="<?php echo $nombre; ?>" disabled>
+            </div>
+            
+            <div class="campo">
+                <label for="fecha">Fecha</label>
+                <input type="date" id="fecha">
+            </div>
+
+            <div class="campo">
+                <label for="hora">Hora</label>
+                <input type="time" id="hora">
+            </div>
+        </form>
+    </div>
+
+    <div id="paso-3" class="seccion">
+        <h2>Resumen</h2>
+        <p class="text-center">Verifica que la información sea correcta</p>
+    </div>
+</div>
