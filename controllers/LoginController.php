f886b773f1aa5af99abffe2b7a9613d9ea15f87a
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-11-25 18:33:32 -0600

    save new password

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -106,7 +106,21 @@ class LoginController {
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // Leer el nuevo password 
-            
+            $password = new Usuario($_POST);
+            $alertas = $password->validarPassword();
+
+            if (empty($alertas)) {
+                $usuario->password = null;
+                $usuario->password = $password->password;
+                $usuario->hashPassword();
+                $usuario->token = null;
+
+                $resultado = $usuario->guardar();
+                if ($resultado) {
+                    header('location: /');
+                }
+                debuguear($usuario);
+            }
         }
         $alertas = Usuario::getAlertas();
 
--- models/Usuario.php
+++ models/Usuario.php
@@ -74,6 +74,18 @@ class Usuario extends ActiveRecord {
         return self::$alertas;
     }
 
+    public function validarPassword() {
+        if (!$this->password) {
+            self::$alertas['error'][] = 'El Password es obligatorio';
+        }
+
+        if (strlen($this->password) < 6) {
+            self::$alertas['error'][] = 'El Password debe tener al menos 6 caracteres';
+        }
+        
+        return self::$alertas;
+    }
+
     // Revisa si el usuario ya existe
     public function existeUsuario() {
         $query = "SELECT * FROM " . self::$tabla . " WHERE email = '" . $this->email . "' LIMIT 1;";
