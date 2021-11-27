2e4748cb7260b4e8218aeaecc86fef19f9cbf203
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-11-26 22:40:09 -0600

    create controller & model

---
+++ controllers/APIController.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace Controllers;
+
+use Model\Servicio;
+use MVC\Router;
+
+class APIController {
+    public static function index(Router $router) {
+        $servicios = Servicio::all();
+
+        echo json_encode($servicios);
+    }
+}
