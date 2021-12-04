5a3abdecdb598c7475bf9236f8867e2e63d1096b
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-12-03 21:05:27 -0600

    save servicios from cita

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -3,6 +3,7 @@
 namespace Controllers;
 
 use Model\Cita;
+use Model\CitaServicio;
 use Model\Servicio;
 use MVC\Router;
 
@@ -15,9 +16,26 @@ class APIController {
 
     public static function guardar() {
         
+        // Almacena la cita y devuelve el id
         $cita = new Cita($_POST);
-
         $resultado = $cita->guardar();
-        echo json_encode($resultado);
+        $id = $resultado['id'];
+
+        // Almacena cita y servicio
+
+        // Almacena los servicios con el Id de la cita
+        $idServicios = explode(',', $_POST['servicios'] );
+        foreach ($idServicios as $idServicio) {
+            $args = [
+                'citaId' => $id,
+                'servicioId' => $idServicio
+            ];
+
+            $citaServicio = new CitaServicio($args);
+            $citaServicio->guardar();
+        }
+
+        // Retornamos una respuesta
+        echo json_encode(['resultado' => $resultado]);
     }
 }
