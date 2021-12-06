91b4f1a79e4b5a843efb826722f4e497831582a4
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-12-05 19:41:47 -0600

    css servicios

--- views/servicios/index.php
+++ views/servicios/index.php
@@ -9,6 +9,15 @@
         <li>
             <p>Nombre: <span><?php echo $servicio->nombre; ?></span></p>
             <p>Precio: <span>$<?php echo $servicio->precio; ?></span></p>
+
+            <div class="acciones">
+                <a href="/servicios/actualizar?id=<?php echo $servicio->id; ?>" class="boton">Actualizar</a>
+
+                <form action="/servicios/eliminar" method="post">
+                    <input type="hidden" name="id" value="<?php echo $servicio->id; ?>"> 
+                    <input type="submit" value="Borrar" class="boton-eliminar">
+                </form>
+            </div>
         </li>
 
     <?php endforeach; ?>
