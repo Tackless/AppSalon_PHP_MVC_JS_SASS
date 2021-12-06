39d8e1131433d47e3d10856f2b471c9e41eb47fe
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-12-05 18:53:20 -0600

    views for servicios

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -7,8 +7,12 @@ use MVC\Router;
 class ServicioController {
     public static function index(Router $router) {
         
-        $router->render('/servicios/index', [
+        if (!isset($_SESSION)) {
+            session_start();
+        }
 
+        $router->render('/servicios/index', [
+            'nombre' => $_SESSION['nombre']
         ]);
 
         
@@ -16,10 +20,17 @@ class ServicioController {
     
     public static function crear(Router $router) {
         
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             
         }
 
+        $router->render('/servicios/crear', [
+            'nombre' => $_SESSION['nombre']
+        ]);
         
     }
     
@@ -29,6 +40,9 @@ class ServicioController {
             
         }
 
+        $router->render('/servicios/actualizar', [
+            'nombre' => $_SESSION['nombre']
+        ]);
         
     }
     
--- views/layout.php
+++ views/layout.php
@@ -6,7 +6,7 @@
     <meta name="description" content="Project with MVC, PHP, SASS, Gulp, Composer">
     <link rel="shortcut icon" href="/build/img/favicon.ico" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" href="build/css/app.css">
+    <link rel="stylesheet" href="/build/css/app.css">
     <title>App Salón</title>
 </head>
 <body>
