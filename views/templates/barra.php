8e5843679627efd71f1332c7cf4aa80adaa7c1ad
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-12-05 18:18:05 -0600

    create menu admin

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -3,3 +3,15 @@
 
     <a href="/logout" class="boton">Cerrar Sesión</a>
 </div>
+
+<?php if (isset($_SESSION['admin'])) : ?>
+
+    <div class="barra-servicios">
+        <a class="boton" href="/admin">Ver Citas</a>
+        <a class="boton" href="/servicios">Ver Servicios</a>
+        <a class="boton" href="/servicios/crear">Crear Servicio</a>
+    </div>
+
+
+
+<?php endif; ?>
